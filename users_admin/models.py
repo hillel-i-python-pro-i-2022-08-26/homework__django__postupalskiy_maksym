cdc60e031b4e05c7541f4b538feeb274d237feba
roflmorries <99510874+roflmorries@users.noreply.github.com>
2022-10-21 22:45:26 +0300

    add custom admin

---
+++ users_admin/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class Admin(AbstractUser):
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Admin'
+# Create your models here.
