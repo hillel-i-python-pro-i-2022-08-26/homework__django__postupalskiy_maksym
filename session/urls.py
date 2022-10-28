c9d4a59a3018a319ea8ea6271013005724f9d1cb
roflmorries <99510874+roflmorries@users.noreply.github.com>
2022-10-28 14:25:49 +0300

    reformat files again

--- session/urls.py
+++ session/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from session import views
 
 
-app_name ="session"
+app_name = "session"
 
-urlpatterns = [
-    path("", views.session_info, name="session")
-]
+urlpatterns = [path("", views.session_info, name="session")]
